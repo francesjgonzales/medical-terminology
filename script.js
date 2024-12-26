75cf1c98337430b2e4d5465766c8d8fb77c1f3b0
francesjgonzales <francesj7837@gmail.com>
2024-12-26 00:11:21 -0700

    fixed form. added new field called category

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-/* const clean = DOMPurify.sanitize(index.html, { WHOLE_DOCUMENT: true });
- */
 
 
-axios.get('https://symphonious-heliotrope-7b674a.netlify.app/getAllSubmitted')
-    .then(function (response) {
-        // handle success
-        const allData = response.data
-        console.log(allData.term);
-
-        let allMedicalTerm = allData.map((medData) => {
-            return `<td>${medData}</td>`
-        })
-        document.querySelector("#showData").innerHTML = allMedicalTerm
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    })
